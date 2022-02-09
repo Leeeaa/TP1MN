7f07ebabeeff2799b8563b5e21d6942746816714
Aiveriwhair <william.isabelle@etu.univ-grenoble-alpes.fr>
2022-02-09 15:29:34 +0100

    Init

---
+++ poly/poly_creux.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "poly.h"
+
+#include <x86intrin.h>
+
---
+++ poly/poly_creux.h
@@ -0,0 +1,8 @@
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
+
+typedef struct {
+  int degre ;
+  float *coeff;
+} polyf_t, *p_polyf_t;
+
