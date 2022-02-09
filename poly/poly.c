7acc4971e9fd0ceb755a075644db9bd39af58873
Aiveriwhair <william.isabelle@etu.univ-grenoble-alpes.fr>
2022-02-09 15:02:37 +0100

    implementation fonction multiplication de polynomes

--- poly/poly.c
+++ poly/poly.c
@@ -154,9 +154,21 @@ float eval_polynome (p_polyf_t p, float x)
 
 p_polyf_t multiplication_polynomes (p_polyf_t p1, p_polyf_t p2)
 {
-  /* p1 * p2 */
-
-  return NULL ;
+  p_polyf_t res = malloc(sizeof(float) * p1->degre + p2->degre);
+  res->degre = p1->degre + p2->degre;
+  for (int i = 0; i < res->degre + 1; i++)
+  {
+    res->coeff[i] = 0;
+  }
+  
+  for (int i = 0; i < p1->degre + 1; i++)
+  {
+    for (int j = 0; j < p2->degre + 1; j++)
+    {
+     res->coeff[i + j] += p1->coeff[i] * p2->coeff[j]; 
+    }
+  }
+  return res;
 }
 
 p_polyf_t puissance_polynome (p_polyf_t p, int n)
@@ -176,6 +188,5 @@ p_polyf_t composition_polynome (p_polyf_t p, p_polyf_t q)
 
   return NULL ;
 }
-
 
 
