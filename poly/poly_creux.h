58bf154a466b9c7b665f254e5d99ab1e640a5c4d
Lea MAIDA <leamaida@icloud.com>
2022-02-17 00:04:14 +0100

    poly_creux

--- poly/poly_creux.h
+++ poly/poly_creux.h
@@ -2,7 +2,29 @@
 #define min(a,b) ((a)<(b)?(a):(b))
 
 typedef struct {
-  int degre ;
+  int nbMonome ;
   float *coeff;
-} polyf_t, *p_polyf_t;
+  int *degre;
+} polyf_creux_t, *p_polyf_creux_t;
+
+p_polyf_creux_t creer_polynome (int degre) ;
+
+void init_polynome (p_polyf_creux_t p, float x) ;
+
+void detruire_polynome (p_polyf_creux_t p) ;
+
+p_polyf_creux_t lire_polynome_float (char *nom_fichier) ;
+
+void ecrire_polynome_float (p_polyf_creux_t p) ;
+
+int egalite_polynome (p_polyf_creux_t p1, p_polyf_creux_t p2) ;
+
+p_polyf_creux_t addition_polynome (p_polyf_creux_t p1, p_polyf_creux_t p2) ;
+
+p_polyf_creux_t multiplication_polynome_scalaire (p_polyf_creux_t p, float alpha) ;
+
+float puiss_float(float x, int deg ) ;
+
+float eval_polynome (p_polyf_creux_t p, float x) ;
+
 
