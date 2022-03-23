fd4a7c3d93eacdba179e20a90f5c233a904c9789
Aiveriwhair <william.isabelle@etu.univ-grenoble-alpes.fr>
2022-03-23 16:56:19 +0100

    Correction erreur code

--- TP2/TPBLAS/src/complexe.c
+++ TP2/TPBLAS/src/complexe.c
@@ -26,7 +26,7 @@ complexe_float_t mult_complexe_float (const complexe_float_t c1, const complexe_
   complexe_float_t r ;
 
   /* 
-     a implementer
+     fait
   */
 
   r.real = 0.0 ;
@@ -43,7 +43,7 @@ complexe_double_t mult_complexe_double (const complexe_double_t c1, const comple
   complexe_double_t r ;
 
   /* 
-     a implementer
+     fait
   */
   
   r.real = 0.0 ;
@@ -60,12 +60,12 @@ complexe_float_t div_complexe_float (const complexe_float_t c1, const complexe_f
 {
   complexe_float_t r ;
 
-  if ((float)c2.real == 0 && (float)c2.imaginary == 0){
-    return NULL;
-  } 
+  // if ((float)c2.real == 0 && (float)c2.imaginary == 0){
+  //   return NULL;
+  // } 
 
   /* 
-     a implementer
+     fait
   */
 
   r.real = 0.0 ;
@@ -84,13 +84,13 @@ complexe_double_t div_complexe_double (const complexe_double_t c1, const complex
   complexe_double_t r ;
 
   /* 
-     a implementer
+     fait
   */
 
   r.real = 0.0 ;
   r.imaginary = 0.0 ;
   
-   denum = c2.real*c2.real + c2.imaginary*c2.imaginary;
+  double denum = c2.real*c2.real + c2.imaginary*c2.imaginary;
   r.real = (c1.real*c2.real + c1.imaginary*c2.imaginary)/ denum;
   r.imaginary = (c1.imaginary*c2.real - c1.real*c2.imaginary)/denum;
 
