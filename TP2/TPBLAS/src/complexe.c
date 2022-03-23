ec4e4f083d5d77fa6d882975d8493789b33a62a1
Aiveriwhair <william.isabelle@etu.univ-grenoble-alpes.fr>
2022-03-23 17:09:38 +0100

    clean le code de complexe.c

--- TP2/TPBLAS/src/complexe.c
+++ TP2/TPBLAS/src/complexe.c
@@ -4,8 +4,7 @@
 complexe_float_t add_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
 {
   complexe_float_t r ;
-
-  r.real = c1.real + c2.real ;
+  r.real = c1.real + c2.real;
   r.imaginary = c1.imaginary + c2.imaginary ;
   
   return r ;
@@ -14,7 +13,6 @@ complexe_float_t add_complexe_float (const complexe_float_t c1, const complexe_f
 complexe_double_t add_complexe_double (const complexe_double_t c1, const complexe_double_t c2)
 {
   complexe_double_t r ;
-
   r.real = c1.real + c2.real ;
   r.imaginary = c1.imaginary + c2.imaginary ;
   
@@ -23,15 +21,7 @@ complexe_double_t add_complexe_double (const complexe_double_t c1, const complex
 
 complexe_float_t mult_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
 {
-  complexe_float_t r ;
-
-  /* 
-     fait
-  */
-
-  r.real = 0.0 ;
-  r.imaginary = 0.0 ;
-
+  complexe_float_t r = {0.0, 0.0};
   r.real = c1.real * c2.real - c1.imaginary * c2.imaginary;
   r.imaginary = c1.real * c2.imaginary + c1.imaginary * c2.real;
   
@@ -40,15 +30,7 @@ complexe_float_t mult_complexe_float (const complexe_float_t c1, const complexe_
 
 complexe_double_t mult_complexe_double (const complexe_double_t c1, const complexe_double_t c2)
   {
-  complexe_double_t r ;
-
-  /* 
-     fait
-  */
-  
-  r.real = 0.0 ;
-  r.imaginary = 0.0 ;
-  
+  complexe_double_t r = {0.0, 0.0};
   r.real = c1.real * c2.real - c1.imaginary * c2.imaginary;
   r.imaginary = c1.real * c2.imaginary + c1.imaginary * c2.real;
 
@@ -58,20 +40,11 @@ complexe_double_t mult_complexe_double (const complexe_double_t c1, const comple
 
 complexe_float_t div_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
 {
-  complexe_float_t r ;
-
   // if ((float)c2.real == 0 && (float)c2.imaginary == 0){
-  //   return NULL;
+  //   return 
   // } 
 
-  /* 
-     fait
-  */
-
-  r.real = 0.0 ;
-  r.imaginary = 0.0 ;
-
-
+  complexe_float_t r = {0.0, 0.0};
   float denum = c2.real*c2.real + c2.imaginary*c2.imaginary;
   r.real = (c1.real*c2.real + c1.imaginary*c2.imaginary)/ denum;
   r.imaginary = (c1.imaginary*c2.real - c1.real*c2.imaginary)/denum;
@@ -81,14 +54,7 @@ complexe_float_t div_complexe_float (const complexe_float_t c1, const complexe_f
 
 complexe_double_t div_complexe_double (const complexe_double_t c1, const complexe_double_t c2)
 {
-  complexe_double_t r ;
-
-  /* 
-     fait
-  */
-
-  r.real = 0.0 ;
-  r.imaginary = 0.0 ;
+  complexe_double_t r = {0.0, 0.0};
   
   double denum = c2.real*c2.real + c2.imaginary*c2.imaginary;
   r.real = (c1.real*c2.real + c1.imaginary*c2.imaginary)/ denum;
