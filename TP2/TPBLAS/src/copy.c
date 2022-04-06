ccd2b9b55c6d4049764718b18a34c504e1629fa6
Ali <Ali.Ghammaz@etu.univ-grenoble-alpes.fr>
2022-04-06 17:43:50 +0200

    copy blas1

--- TP2/TPBLAS/src/copy.c
+++ TP2/TPBLAS/src/copy.c
@@ -1,4 +1,5 @@
 #include "mnblas.h"
+#include "complexe.h"
 
 void mncblas_scopy(const int N, const float *X, const int incX, 
                  float *Y, const int incY)
@@ -17,18 +18,42 @@ void mncblas_scopy(const int N, const float *X, const int incX,
 void mncblas_dcopy(const int N, const double *X, const int incX, 
                  double *Y, const int incY)
 {
+  register unsigned int i = 0 ;
+  register unsigned int j = 0 ;
 
+  for (; ((i < N) && (j < N)) ; i += incX, j += incY)
+    {
+      Y [j] = X [i] ;
+    }
+
+  return ;
 }
 
 void mncblas_ccopy(const int N, const void *X, const int incX, 
 		                    void *Y, const int incY)
 {
+  register unsigned int i = 0 ;
+  register unsigned int j = 0 ;
 
+  for (; ((i < N) && (j < N)) ; i += incX, j += incY)
+    {
+      ((complexe_float_t*)Y)[j] = ((complexe_float_t*)X)[i];
+    }
+
+  return ;
 }
 
 void mncblas_zcopy(const int N, const void *X, const int incX, 
 		                    void *Y, const int incY)
 {
+  register unsigned int i = 0 ;
+  register unsigned int j = 0 ;
 
+  for (; ((i < N) && (j < N)) ; i += incX, j += incY)
+    {
+      ((complexe_float_t*)Y)[j] = ((complexe_float_t*)X)[i];
+    }
+
+  return ;
 }
 
