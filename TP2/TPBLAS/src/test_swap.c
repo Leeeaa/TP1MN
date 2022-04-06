a7d6a5b7f88750ecff7a7c40dfce62be582ed9d3
Aiveriwhair <william.isabelle@etu.univ-grenoble-alpes.fr>
2022-04-06 18:05:08 +0200

    test compilation fichier test_swap.c

--- TP2/TPBLAS/src/test_swap.c
+++ TP2/TPBLAS/src/test_swap.c
@@ -2,8 +2,15 @@
 
 int main()
 {
+
+    float X[10] = {0,1,2,3,4,5,6,7,8,9};
+    float Y[10];
+
+    mncblas_sswap(10,X,1,Y,1);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Valeur %d : %d", i, Y[i]);
+    }
     
-
-
     return 0;
 }
