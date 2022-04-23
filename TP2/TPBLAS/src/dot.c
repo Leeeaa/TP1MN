e40b4a5874be09f67d12b410c076c62e6a0eb38e
Lea MAIDA <leamaida@icloud.com>
2022-04-23 13:35:45 +0200

    pas longtemps avant l'abandon (q9)

--- TP2/TPBLAS/src/dot.c
+++ TP2/TPBLAS/src/dot.c
@@ -1,4 +1,5 @@
 #include "mnblas.h"
+#include "complexe.h"
 #include <stdio.h>
 
 /*
@@ -38,17 +39,38 @@ float mncblas_sdot(const int N, const float *X, const int incX,
 double mncblas_ddot(const int N, const double *X, const int incX, 
                  const double *Y, const int incY)
 {
-  /*
-     a completer
-  */
+  //done
+  register unsigned int i = 0 ;
+  register unsigned int j = 0 ;
+  double dot = 0.0 ;
+
   
-  return 0.0;
+  for (i = 0 ; i < N ; i += incX)
+    {
+      dot += X [i] * Y [j] ;
+      j+=incY ;
+    }
+
+  return dot ;
 }
 
 void   mncblas_cdotu_sub(const int N, const void *X, const int incX,
                        const void *Y, const int incY, void *dotu)
 {
-  /* a completer */
+  //todo
+  register unsigned int i = 0 ;
+  register unsigned int j = 0 ;
+
+  //complexe_float_t sum = {0,0}; // ou (complexe_float_t){0,0} ?
+
+  for (i = 0 ; i < N ; i += incX)
+    {
+      //sum = add_complexe_float(((complexe_float_t*)X)[i],((complexe_float_t*)Y)[j]);
+
+      //*((complexe_float_t*)(dotu)) = add_complexe_float(sum,*((complexe_float_t)dotu));
+      j+=incY ;
+
+    }
   
   return ;
 }
--- TP2/TPBLAS/src/test_dot.c
+++ TP2/TPBLAS/src/test_dot.c
@@ -3,9 +3,34 @@
 
 int main()
 {
+	/*dot : The dot functions compute the dot product dot of two distributed REAL vectors
+	 res = I1*J1+I2*J2+...+In*Jn
+	*/
+
+    // ------------ test fonction sdot --------------- 
+
+    float Xf[10] = {0,1,2,3,4,5,6,7,8,9};
+    //float Xfbis[20] = {0,1,2,3,4,5,6,7,8,9,20,21,22,23,24,25,26,27,28,29};
+    float Yf[10] = {10,11,12,13,14,15,16,17,18,19};
+
+        
+    printf("resultat test sdot : %f \n", mncblas_sdot(10,Xf,1,Yf,1));
+    //printf ("resultat: %f \n", mncblas_sdot(10,Xfbis,2,Yf,1));
+	
+
+	// ------------ test fonction ddot --------------- 	
+
+	double Xd[10] = {0,1,2,3,4,5,6,7,8,9};
+    double Yd[10] = {10,11,12,13,14,15,16,17,18,19};
+
+    printf("resultat test ddot : %f \n", mncblas_ddot(10,Xd,1,Yd,1));
 
 
-	printf("test compilation \n");
+    /*dotu : The dot functions compute the dot product dot of two distributed COMPLEX vectors
+	 res = I1*J1+I2*J2+...+In*Jn
+	*/
+
+
 
 	return 0;
 
