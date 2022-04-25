5186078c8af9df70e5f058a088896acd0787af0e
Lea MAIDA <leamaida@icloud.com>
2022-04-25 22:38:07 +0200

    asum.c

--- TP2/TPBLAS/src/asum.c
+++ TP2/TPBLAS/src/asum.c
@@ -3,75 +3,66 @@
 #include <stdio.h>
 #include <math.h>
 
-float mncblas_sasum(const int N, const float *X, const int incX, 
-                 const float *Y, const int incY)
+
+//The functions p?asum compute the sum of the magnitudes of elements of a distributed vector sub(x),
+
+float mncblas_sasum(const int N, const float *X, const int incX)
 {
   register unsigned int i = 0 ;
-  register unsigned int j = 0 ;
   float sasum = 0.0 ;
   float sum = 0;
   
   for (i = 0 ; i < N ; i += incX)
     {
-      sum = X[i]*X[i] + Y[j]*Y[j];
-      sasum += Math.pow(sum,0.5) ;
-      j+=incY ;
+      sum = X[i]*X[i] ;
+      sasum += pow(sum,0.5) ;
     }
 
   return sasum ;
 }
 
-double mncblas_dasum(const int N, const double *X, const int incX, 
-                 const double *Y, const int incY)
+double mncblas_dasum(const int N, const double *X, const int incX)
 {
   register unsigned int i = 0 ;
-  register unsigned int j = 0 ;
   double dasum = 0.0 ;
   double sum = 0;
   
   for (i = 0 ; i < N ; i += incX)
     {
-      sum = X[i]*X[i] + Y[j]*Y[j];
-      sasum += Math.pow(sum,0.5) ;
-      j+=incY ;
+      sum = X[i]*X[i];
+      dasum += pow(sum,0.5) ;
     }
 
   return dasum ;
 }
 
-void   mncblas_casum(const int N, const void *X, const int incX,
-                       const void *Y, const int incY, void *dotu)
+float mncblas_casum(const int N, const void *X, const int incX)
 {
   //done
   register unsigned int i = 0 ;
-  register unsigned int j = 0 ;
   float casum = 0.0 ;
   float sum = 0;
   
   for (i = 0 ; i < N ; i += incX)
     {
-      sum = mult_complexe_float(((complexe_float*)X)[i]), (((complexe_float_t*)Y)[j]);
-      sasum += Math.pow(sum,0.5) ;
-      j+=incY ;
+      sum = (((complexe_float_t*)X)[0].real) * (((complexe_float_t*)X)[0].real) + (((complexe_float_t*)X)[0].imaginary) * (((complexe_float_t*)X)[0].imaginary);
+      casum += pow(sum,0.5) ;
     }
 
   return casum ;
 }
 
-void   mncblas_zasum(const int N, const void *X, const int incX,
-                       const void *Y, const int incY, void *dotc)
+double mncblas_zasum(const int N, const void *X, const int incX)
 {
   
   register unsigned int i = 0 ;
-  register unsigned int j = 0 ;
   double zasum = 0.0 ;
   double sum = 0;
   
   for (i = 0 ; i < N ; i += incX)
     {
-      sum = mult_complexe_float(((complexe_double*)X)[i]), (((complexe_double*)Y)[j]);
-      sasum += Math.pow(sum,0.5) ;
-      j+=incY ;
+      sum = (((complexe_double_t*)X)[0].real) * (((complexe_double_t*)X)[0].real) + (((complexe_double_t*)X)[0].imaginary) * (((complexe_double_t*)X)[0].imaginary);
+      zasum += pow(sum,0.5) ;
     }
 
   return zasum ;
